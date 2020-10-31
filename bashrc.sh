1ad3de440f9cfc12284c08d5ae152dfddb2fb879
Lucie Cupcakes <cirno9moe@gmail.com>
2020-10-31 13:11:37 +0000

    fast_push at 20201031_131137

--- bashrc.sh
+++ bashrc.sh
@@ -1,8 +1,25 @@
 #!/usr/bin/bash
 [[ $- != *i* ]] && return
 
+_is_running_in_chroot() {
+	awk 'BEGIN{exit_code=1} $2 == "/" {exit_code=0} END{exit exit_code}' /proc/mounts
+	test $? -eq 1
+}
+
 export PS1="\[\033[38;5;225m\]\h\[$(tput sgr0)\]\[\033[38;5;15m\] \[$(tput sgr0)\]\[\033[38;5;189m\]\w\[$(tput sgr0)\]\[\033[38;5;15m\] % \[$(tput sgr0)\]"
 #PS1='[\u@\h \W]\$ '
+
+if _is_running_in_chroot; then
+	if [ -f "/etc/os-release" ]; then
+		distro_name=$(cat /etc/os-release | grep ID | head -1)
+		distro_name_ar=(${distro_name//=/ })
+		export PS1="\[\033[38;5;172m\]${distro_name_ar[1]}\[$(tput sgr0)\] $PS1"
+		unset distro_name
+		unset distro_name_ar
+	else
+		export PS1="\[\033[38;5;172m\]chroot\[$(tput sgr0)\] $PS1"
+	fi
+fi
 
 [ -d /usr/local/go/bin ] && export PATH=$PATH:/usr/local/go/bin
 [ -d "$HOME/bin" ] && export PATH="$HOME/bin:$PATH"
@@ -53,6 +70,7 @@ activate_pyenv() {
 }
 
 export QEMURUN_VM_PATH="$HOME/VM"
+export DOTNET_CLI_TELEMETRY_OPTOUT=1
 
 alias ls='ls --color=auto'
 alias irssi='irssi -n lucie_ow --config=$HOME/.config/irssi/irssi.conf --home=$HOME/.config/irssi'
