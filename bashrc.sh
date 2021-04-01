88b7823063d690912169231295c91adba2254aeb
Lucie Cupcakes <lucie_linux@protonmail.com>
2021-04-01 08:07:14 -0300

    fast_commit at 20210401_080714

--- bashrc.sh
+++ bashrc.sh
@@ -40,7 +40,7 @@ export QEMURUN_VM_PATH="$HOME/VM"
 export CC=gcc
 
 alias ls='ls --color=auto'
-alias irssi='irssi -n lucie_ow --config=$HOME/.config/irssi/irssi.conf --home=$HOME/.config/irssi'
+alias irssi='irssi -n lucie-cupcakes --config=$HOME/.config/irssi/irssi.conf --home=$HOME/.config/irssi'
 
 alias cc_flags_debug='$CC -g -O0 -D DEBUG -mtune=generic -fsanitize=address,leak -std=c99 -pedantic -Wall -Werror -Wextra'
 alias cc_flags='$CC -Os -s -pipe -mtune=generic -std=c99 -pedantic -Wall -Werror -Wextra'
