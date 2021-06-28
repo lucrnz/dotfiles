966118e2a45985e3475fe9126b3568d9159be09c
Lucie Cupcakes <lucie_linux@protonmail.com>
2021-06-28 11:50:41 -0300

    fast_commit at 20210628_115041

--- bashrc.sh
+++ bashrc.sh
@@ -29,12 +29,8 @@ export PS1="\[\033[38;5;225m\]\h\[$(tput sgr0)\]\[\033[38;5;15m\] \[$(tput sgr0)
 prepend_path "/usr/bin/watcom/binl"
 prepend_path "/usr/local/go/bin"
 prepend_path "/snap/bin"
-prepend_path "/snap/bin"
 prepend_path "$HOME/.local/bin"
 prepend_path "$HOME/.local/share/go/bin"
-prepend_path "$HOME/.conf_files/scripts"
-prepend_path "$HOME/.conf_files/mono_scripts/sh"
-prepend_path "$HOME/.conf_files/cc_scripts/bin"
 prepend_path "$HOME/.local/share/nimble/bin"
 prepend_path "$HOME/.local/share/pyston"
 
@@ -42,7 +38,12 @@ test -f "$HOME/.cargo/env" && source "$HOME/.cargo/env"
 if test -d "$HOME/.local/share/dotnet"; then
 	export PATH="$HOME/.local/share/dotnet:$PATH"
 	export DOTNET_ROOT="$HOME/.local/share/dotnet"
+	export DOTNET_CLI_TELEMETRY_OPTOUT=1
 fi
+
+prepend_path "$HOME/.conf_files/scripts"
+prepend_path "$HOME/.conf_files/mono_scripts/sh"
+prepend_path "$HOME/.conf_files/cc_scripts/bin"
 
 export EDITOR=nano
 export VISUAL=less
@@ -59,24 +60,6 @@ alias cc_flags='$CC -Os -s -pipe -mtune=generic -std=c99 -pedantic -Wall -Werror
 xz_full_autism() { xz -z -9 -e -T $(nproc) -v -v -v -k $@ ; }
 fork_muted() { $@ >/dev/null 2>&1& }
 alias _fm="fork_muted"
-
-ramdisk() {
-	target="/tmp/ramdisk"
-	if [ -d $target ]; then
-		echo "$target already exists??"
-	else
-		uid=$(id -u)
-		gid=$(id -g)
-		mkdir $target &&
-		sudo mount -t tmpfs -o size=$1,uid=${uid},gid=${gid} ramdisk_user /tmp/ramdisk
-	fi
-}
-
-find_pendrive() {
-	fd=$(sudo fdisk -l)
-	echo "$fd" | grep 7864320000
-	echo "$fd" | grep 30979129344
-}
 
 if cmd_exists dpkg; then
 	apt_autopurge() {
