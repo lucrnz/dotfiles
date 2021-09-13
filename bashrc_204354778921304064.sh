bc93ad5fe14f3f678ed3391e6185d7f6d42552d6
Lucie Cupcakes <lucie_linux@protonmail.com>
2021-09-13 03:15:17 +0000

    fast_commit at 20210913_031517

--- bashrc_204354778921304064.sh
+++ bashrc_204354778921304064.sh
@@ -4,3 +4,7 @@ export GOBIN=/usr/local/go/bin:$HOME/go/bin
 alias ls="ls --color=auto"
 
 prepend_path "$HOME/local/nvim/bin"
+export PYENV_ROOT="$HOME/.pyenv"
+export PATH="$PYENV_ROOT/bin:$PATH"
+eval "$(pyenv init --path)"
+
