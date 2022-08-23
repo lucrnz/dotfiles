a249a49c6705ea65aa17d0a3275f9989bee3cbe5
Lucien <lucie_linux@protonmail.com>
2022-08-23 20:59:51 +0000

    fast_commit at 20220823_205951

--- bashrc.d/bun.sh
+++ bashrc.d/bun.sh
@@ -1,6 +1,6 @@
-# Bun 
-if test -d "$HOME/.bun"; then
-	export BUN_INSTALL="$HOME/.bun"
+# Bun
+if test -d "$HOME/.local/share/bun"; then
+	export BUN_INSTALL="$HOME/.local/share/bun"
 	export PATH="$BUN_INSTALL/bin:$PATH"
 fi
 
