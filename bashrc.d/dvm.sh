5e2e1587283e45d8b89c10b6a715fe6d32682f93
Lucien <lucie_linux@protonmail.com>
2022-09-15 11:06:48 -0300

    fast_commit at 20220915_110648

--- bashrc.d/dvm.sh
+++ bashrc.d/dvm.sh
@@ -1,8 +1,12 @@
-# Deno version manager
+# Deno version manager & Deno packages
 # curl -fsSL https://dvm.deno.dev | sh
 
 if test -d "$HOME/.dvm"; then
 	export DVM_DIR="$HOME/.dvm"
 	export PATH="$DVM_DIR/bin:$PATH"
+
+	if test -d "$HOME/.deno/bin"; then
+		export PATH="$HOME/.deno/bin:$PATH"
+	fi
 fi
 
