9cbee0a0cba016ab80e44877f33c0c0ca08eb67d
Lucien <lucie_linux@protonmail.com>
2022-09-15 10:14:51 -0300

    fast_commit at 20220915_101451

--- bashrc.sh
+++ bashrc.sh
@@ -36,7 +36,3 @@ test -f "$HOME/.conf_files/bashrc_${HOSTNAME}.sh" && \
 	source "$HOME/.conf_files/bashrc_${HOSTNAME}.sh"
 
 unset _include_all
-
-export NVM_DIR="$HOME/.nvm"
-[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
-[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
