eb2072f27e137607df28e19a36b691f61a713aad
lucdev <me@lucdev.net>
2023-01-30 00:08:19 -0300

    Ahaha, this is just a test..

---
+++ install.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+## ---------------------------------------
+# I made this script for GiHhub Codespaces
+# I have no idea if it works fine for a normal environment
+## ---------------------------------------
+
+## --- 
+# To test I will first just log what is going on..
+##
+
+echo $@ >> $HOME/log.txt
+pwd > $HOME/log.txt
