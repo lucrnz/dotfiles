9340006eaf1a7d5133b36f1ca17a077956d46dfd
lucdev <me@lucdev.net>
2023-05-03 21:51:33 -0300

    neovim: setup toggleterm

--- nvim/lua/plugins/toggleterm.lua
+++ nvim/lua/plugins/toggleterm.lua
@@ -1,6 +1,11 @@
 return {
   {
     "akinsho/toggleterm.nvim",
+    version = "*",
+    opts = {
+      direction = "horizontal",
+      open_mapping = [[<C-t>]],
+    },
     config = true,
   },
 }
