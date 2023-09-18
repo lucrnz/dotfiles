015552c12a7041467e0559d33e9c96dd74c96f12
lucdev <me@lucdev.net>
2023-09-17 23:28:46 -0300

    Refactor init_direnv script and add Python support

---
+++ data/direnv/rust/flake.nix
@@ -0,0 +1,12 @@
+{
+  inputs.nixpkgs.url = "github:nixos/nixpkgs/master";
+  outputs = { self, nixpkgs, flake-utils }: flake-utils.lib.eachDefaultSystem (system:
+    let
+      pkgs = import nixpkgs { inherit system; };
+    in {
+      devShells.default = pkgs.mkShell {
+        buildInputs = [ pkgs.rustc pkgs.cargo ];
+      };
+    }
+  );
+}
