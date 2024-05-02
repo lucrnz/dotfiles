302241726a2c027942ac523ac6a0cf46d09fcef8
lucdev <me@lucdev.net>
2024-05-02 02:03:25 -0300

    Remove this stuff

--- cli-apps-available/go.sh
+++
@@ -1,6 +0,0 @@
-export GOROOT="$HOME/.local/share/go"
-export GOPATH="$HOME/go"
-export PATH="$GOROOT/bin:$PATH"
-
-test -d "$GOPATH/bin" && export PATH="$GOPATH/bin:$PATH"
-
--- cli-apps-available/vscode-cli.sh
+++
@@ -1,3 +0,0 @@
-# vscode-cli
-export PATH=$HOME/.local/share/vscode-cli:$PATH
-
